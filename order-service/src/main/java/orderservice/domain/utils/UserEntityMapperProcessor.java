448b9abb234cae0a0f1ede8f9d96f88b58f676e1
IvanZvyagin <ivan.zvyagin20@gmail.com>
2026-03-02 20:49:46 +0300

    Переработка маппера для юзера Добавление в энтити поля эмэйл Возврат правильного UserDTO с эмэйлом Добавил AuthResponse, но думаю бесполезно

---
+++ order-service/src/main/java/orderservice/domain/utils/UserEntityMapperProcessor.java
@@ -0,0 +1,15 @@
+package orderservice.domain.utils;
+
+import orderservice.api.dto.RegisterUserRequestDto;
+import orderservice.api.dto.UserDto;
+import orderservice.domain.entity.UserEntity;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = MappingConstants.ComponentModel.SPRING)
+public interface UserEntityMapper {
+    UserEntity toEntity(UserEntity user);
+
+    UserDto toUserDto(UserEntity userEntity);
+}
