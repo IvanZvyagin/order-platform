0327b7605c64184f69d50d6328e98192adfcb4a4
IvanZvyagin <ivan.zvyagin20@gmail.com>
2026-03-29 18:21:19 +0300

    Изменен принцип генерации токенов

--- notification-service/src/main/java/notificationservice/repository/NsOrderRepository.java
+++ notification-service/src/main/java/notificationservice/repository/NsOrderRepository.java
@@ -18,38 +18,4 @@ public interface NsOrderRepository extends R2dbcRepository<NsOrderEntity, Long> 
     Flux<NsOrderEntity> findAllByUserId(Long userId);
 
     Flux<NsOrderEntity> findByStatus(String status);
-
-    @Query("SELECT * FROM orders WHERE created_at BETWEEN $1 and $2 ORDER BY created_at DESC ")
-    Flux<NsOrderEntity> findOrdersInPeriod(LocalDateTime start, LocalDateTime end);
-
-    @Query("""
-        SELECT user_id, COUNT(*) as order_count, SUM(total_price) as total_spent 
-        FROM orders 
-        GROUP BY user_id
-        """)
-    Flux<UserStats> getUserStatistic();
-
-    @Query("""
-        SELECT product_id, COUNT(*) as order_count, SUM(quantity) as total_quantity, SUM(total_amount) as total_revenue
-        FROM orders
-        GROUP BY product_id
-""")
-    Flux<ProductStats> getProductsStatistic();
-
-    @Getter
-    @Setter
-    class UserStats{
-        private Long userId;
-        private Long orderCount;
-        private BigDecimal totalSpent;
-    }
-
-    @Getter
-    @Setter
-    class ProductStats{
-        private Long productId;
-        private Long orderCount;
-        private Long totalQuantity;
-        private BigDecimal totalRevenue;
-    }
 }
